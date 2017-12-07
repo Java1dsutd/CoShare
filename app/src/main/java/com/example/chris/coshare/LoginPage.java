4130c2590b3fae2f73041040b01a333ea060c1a0
nightzsky <33797097+nightzsky@users.noreply.github.com>
2017-12-08 01:46:01 +0800

    QRCodeScanner Added

--- app/src/main/java/com/example/chris/coshare/LoginPage.java
+++ app/src/main/java/com/example/chris/coshare/LoginPage.java
@@ -2,6 +2,7 @@ package com.example.chris.coshare;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,59 +16,66 @@ public class LoginPage extends AppCompatActivity {
     Button register;
     Button submit;
 
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
-        submit=(Button)findViewById(R.id.submitB);
-        password = (EditText)findViewById(R.id.inputPw);
-        username = (EditText)findViewById(R.id.inputUser);
+        submit = (Button) findViewById(R.id.submitB);
+        password = (EditText) findViewById(R.id.inputPw);
+        username = (EditText) findViewById(R.id.inputUser);
         register = (Button) findViewById(R.id.signUp);
 
     }
 
-    public void submithere(View view){
+    public void submithere(View view) {
 
-        String passwordText =password.getText().toString();
+        String passwordText = password.getText().toString();
         String usernameText = username.getText().toString();
-        if(!passwordText.trim().equals("") || !usernameText.trim().equals("")){
+        if (!passwordText.trim().equals("") || !usernameText.trim().equals("")) {
             //correct login
-            if(passwordText.equals("123") && usernameText.equals("123") ){
-                Intent intent =new Intent(LoginPage.this, HomePage.class);
+            if (passwordText.equals("123") && usernameText.equals("123")) {
+                Intent intent = new Intent(LoginPage.this, HomePage.class);
+                sharedPreferences = getApplicationContext().getSharedPreferences("Details", 0); //store user's name in shared preferences, 0 is private mode
+                editor = sharedPreferences.edit(); //get Editor to edit sharedPreferences
+                editor.putString("Username", usernameText); // put the username into sharedPreferences
+                editor.commit();
                 startActivity(intent);
-                Toast.makeText(LoginPage.this, "Login Success", Toast. LENGTH_SHORT).show();
+                Toast.makeText(LoginPage.this, "Login Success", Toast.LENGTH_SHORT).show();
+
             }
 
             //username filled password blank
-            else if (passwordText.trim().equals("")  && usernameText.equals("123") ){
-                Toast.makeText(LoginPage.this, "Please input password", Toast. LENGTH_SHORT).show();
+            else if (passwordText.trim().equals("") && usernameText.equals("123")) {
+                Toast.makeText(LoginPage.this, "Please input password", Toast.LENGTH_SHORT).show();
             }
 
             //password filled username blank
-            else if (usernameText.trim().equals("")  && passwordText.equals("123") ){
-                Toast.makeText(LoginPage.this, "Please input username", Toast. LENGTH_SHORT).show();
+            else if (usernameText.trim().equals("") && passwordText.equals("123")) {
+                Toast.makeText(LoginPage.this, "Please input username", Toast.LENGTH_SHORT).show();
             }
 
             //username correct but password wrong
-            else if (usernameText.equals("123")  && !passwordText.equals("123") ){
-                Toast.makeText(LoginPage.this, "Wrong Password", Toast. LENGTH_SHORT).show();
+            else if (usernameText.equals("123") && !passwordText.equals("123")) {
+                Toast.makeText(LoginPage.this, "Wrong Password", Toast.LENGTH_SHORT).show();
             }
 
             //password correct but username wrong
-            else if (!usernameText.equals("123")  && passwordText.equals("123") ){
-                Toast.makeText(LoginPage.this, "Wrong Username", Toast. LENGTH_SHORT).show();
+            else if (!usernameText.equals("123") && passwordText.equals("123")) {
+                Toast.makeText(LoginPage.this, "Wrong Username", Toast.LENGTH_SHORT).show();
             }
 
             //both username and password wrong
-            else if (!usernameText.equals("123")  && !passwordText.equals("123") ){
-                Toast.makeText(LoginPage.this, "Wrong Username and Password", Toast. LENGTH_SHORT).show();
+            else if (!usernameText.equals("123") && !passwordText.equals("123")) {
+                Toast.makeText(LoginPage.this, "Wrong Username and Password", Toast.LENGTH_SHORT).show();
             }
 
-        } else{
+        } else {
             Toast.makeText(LoginPage.this, "Empty Username and Password", Toast.LENGTH_SHORT).show();
         }
-
     }
 
 

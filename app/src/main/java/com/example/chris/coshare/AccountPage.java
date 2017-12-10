57be985ad666df4e7adf922245dbdd41d5a973a9
InfinityMichelle <infinityisyou96@gmail.com>
2017-12-10 14:32:00 +0800

    Rewards upon points=1000

--- app/src/main/java/com/example/chris/coshare/AccountPage.java
+++ app/src/main/java/com/example/chris/coshare/AccountPage.java
@@ -1,8 +1,13 @@
 package com.example.chris.coshare;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -35,6 +40,9 @@ public class AccountPage extends AppCompatActivity {
     TextView points;
     TextView locationsVisited;
 
+    TextView rewards;
+    Button claim;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +59,9 @@ public class AccountPage extends AppCompatActivity {
         welcomeMsg=findViewById(R.id.welcomeUser);
         points=findViewById(R.id.totalPointsNum);
         locationsVisited=findViewById(R.id.tablesSharedNum);
+
+        rewards = findViewById(R.id.rewardsTV);
+        claim  = findViewById(R.id.claimReward);
 
         be=new backend();
         DBrefLocations.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -76,8 +87,49 @@ public class AccountPage extends AppCompatActivity {
                 }
                 locationsVisited.setText(Long.toString(totalLocations));
 
-            }
 
+                claim.setVisibility(View.INVISIBLE);
+
+                if (userPoints == 1000){
+                    //button show
+                    rewards.setText("Claim your rewards!");
+                    claim.setVisibility(View.VISIBLE);
+                    claim.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            AlertDialog alertDialog = new AlertDialog.Builder(AccountPage.this).create();
+                            LayoutInflater factory = LayoutInflater.from(AccountPage.this);
+                            final View view = factory.inflate(R.layout.alertdialog, null);
+                            alertDialog.setView(view);
+                            alertDialog.setTitle("Congratulations!");
+                            alertDialog.setMessage("Show this e-voucher to claim your free Starbucks!");
+                            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+                            alertDialog.show();
+
+
+
+                        }
+                    });
+
+                    //reset to zero
+
+                } else if (userPoints < 1000){
+                    Integer userPts = (int) (long) userPoints;
+                    int remainingPts = 1000 - userPts;
+                    rewards.setText(remainingPts + " points \n more to claim your reward!");
+                } else if (userPoints > 1000){
+                    Integer userPts = (int) (long) userPoints;
+                    int remainingPts = 2000 - userPts;
+                    rewards.setText(remainingPts + " points \n more to claim your reward!");
+                }
+
+
+            }
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
